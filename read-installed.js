npm/read-installed

commit c46e5a8223f43842c2fb933ea9bb90c7efac6f91

Fixes peerDependencies being allowed even if they are devDeps
If a devDep is a peerDependency then it should be marked as extraneous
if `dev` is set to `false`.

This fixes npm/npm#5586

read-installed.js
@@ -330,7 +330,7 @@ function findUnmet (obj, opts) {
     } else if (!semver.satisfies(dependency.version, peerDeps[d], true)) {
       dependency.peerInvalid = true
     } else {
-      dependency.extraneous = false
+      dependency.extraneous = dependency.extraneous || false
     }
   })
 

test/grandparent-peer-dev.js
@@ -1,0 +1,20 @@
+var readInstalled = require('../read-installed.js')
+var test = require('tap').test
+var path = require('path');
+
+function allValid(t, map) {
+  var deps = Object.keys(map.dependencies || {})
+  deps.forEach(function (dep) {
+    t.ok(map.dependencies[dep].extraneous, 'dependency ' + dep + ' of ' + map.name + ' is extraneous')
+  })
+}
+
+test('grandparent dev peer dependencies should be extraneous', function(t) {
+  readInstalled(
+    path.join(__dirname, 'fixtures/grandparent-peer-dev'),
+    { log: console.error },
+    function(err, map) {
+      allValid(t, map)
+      t.end()
+    })
+})

commit 2fb48a486c78751adc379ec2c381bc7aba3a396d

Peer dep extraneousness inherits from peer
Fixes regression introduced in c46e5a8, where peer deps were improperly
marked as extraneous if they are only depended on as a peerDependency.

This inheritance is important, because a situation can arise where a
devDependency (in non-dev mode) will be marked as extraneous.  However,
it may have a cyclic peer dependency, causing it to be marked as
required.

This way, at least one of the peer dependencies has to be not marked as
extraneous, or else they all will be.

read-installed.js
@@ -330,7 +330,7 @@ function findUnmet (obj, opts) {
     } else if (!semver.satisfies(dependency.version, peerDeps[d], true)) {
       dependency.peerInvalid = true
     } else {
-      dependency.extraneous = dependency.extraneous || false
+      dependency.extraneous = obj.extraneous || false
     }
   })
 