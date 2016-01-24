8adb6ca9fd9a285e1eee62765419bf0c1cb2ec91
amir-arad <amira@wix.com>
2016-01-24 23:52:53 +0200

    add failing test for peerinvalid details

--- test/grandparent-peer-invalid.js
+++ test/grandparent-peer-invalid.js
@@ -1,1 +1,13 @@
+var readInstalled = require('../read-installed.js')
+var test = require('tap').test
+var path = require('path');
 
+test('unsatisfied peer dependencies leave error data', function(t) {
+	readInstalled(
+		path.join(__dirname, 'fixtures/grandparent-peer-invalid'),
+		{ log: console.error },
+		function(err, map) {
+			t.match(map.dependencies.framework.peerInvalid, ['plugin-wrapper@0.0.0->plugin@0.0.0'], 'dependency framework of ' + map.name + ' is peerInvalid with data on breaking peerdependency')
+			t.end()
+		})
+})
